643ddf15aa121ea486c653719d5eb1f0030de2fc
Batuhan Baştürk <batuhanbasturk636@gmail.com>
2023-07-29 16:49:25 +0300

    speedometer rotation

--- fighter-aircraft/fighter-aircraft-client/src/components/Speedometer.jsx
+++ fighter-aircraft/fighter-aircraft-client/src/components/Speedometer.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { ReactComponent as Circle } from "../svgs/speedometer-circle.svg";
+import { ReactComponent as Arrow } from "../svgs/speedometer-arrow.svg";
 
 const Speedometer = ({ speed }) => {
   const arrowRotation = -120 + (speed * 240) / 100; // Calculate rotation angle for arrow
 
+  const containerStyle = {
+    position: "relative",
+    width: "200px",
+    height: "200px",
+  };
+
+  const circleStyle = {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+  };
+
+  const arrowStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: `translate(-50%, -50%) rotate(${arrowRotation}deg)`,
+  };
+
   return (
     <div>
-      <div>{speed} mph</div>
+      <div>{speed} KM/H</div>
+      <div style={containerStyle}>
+        <Circle style={circleStyle} />
+        <Arrow style={arrowStyle} />
+      </div>
     </div>
   );
 };
