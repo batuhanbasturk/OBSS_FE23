0948ebf21c0b116811cc34892e4639bd465ef1c4
Batuhan Baştürk <batuhanbasturk636@gmail.com>
2023-07-29 16:48:51 +0300

    base plane rotate

--- fighter-aircraft/fighter-aircraft-client/src/components/Plane.jsx
+++ fighter-aircraft/fighter-aircraft-client/src/components/Plane.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { ReactComponent as PlaneSvg } from "../svgs/294109.svg";
 const Plane = ({ angle }) => {
-  return <div>Plane Angle: {angle}</div>;
+  return (
+    <div>
+      <div>Plane Angle: {angle}</div>
+      <PlaneSvg style={{ transform: `rotate(${angle}deg)` }} />
+    </div>
+  );
 };
 
 export default Plane;
